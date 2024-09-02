9cf768c6640cf9043ed313b7d40e22fcd62c6fbb
Andrey Plahtiy <aplaxtiy@gmail.com>
2024-09-02 19:24:23 +0300

    update neyvan site

--- assets/js/mail.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function() {
-//     $('#sendMessageButton').click(function() {
-//         $('#contactForm').attr('action',
-//                        'mailto:neyvan.development@gmail.com?subject=' +
-//                        $('#subject').val() + '&body=' + $('#message').val());
-//         $('#contactForm').submit();
-//     });
-// });
